fd97a80f46ba1f769165ec956a99383ca411b9be
pakwesi <pakwesiinfornie1@gmail.com>
2019-10-04 03:38:35 +0200

    Pushing ip adress the day away

--- personalsite/settings/dev.py
+++ personalsite/settings/dev.py
@@ -7,7 +7,7 @@ DEBUG = True
 SECRET_KEY = '(4vm3f@@9sg_opu)-l&6e$4_hj*%%oe+b)#*rjbt187b!3jz$a'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['67.205.181.146','*',]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
--- personalsite/settings/production.py
+++ personalsite/settings/production.py
@@ -2,6 +2,8 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['67.205.181.146']
+
 try:
     from .local import *
 except ImportError:
